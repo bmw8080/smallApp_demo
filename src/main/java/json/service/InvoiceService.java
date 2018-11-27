f0c2c5485a38a9e5f59cc3590fdf656620d2b6f4
gjh31720 <gjh31720@fjtic.cn>
2018-11-27 11:20:34 +0800

    init xyinvoice

---
+++ src/main/java/json/service/InvoiceService.java
@@ -0,0 +1,26 @@
+package json.service;
+
+
+import json.model.Invoice;
+import org.springframework.scheduling.annotation.Async;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public interface InvoiceService {
+    List<Invoice> Query();
+
+    Invoice addData(Invoice temp);
+    
+
+    Object findAllInvoice(int pageNum, int pageSize);
+
+
+    List<Invoice> checkRepeat(List<Invoice> receptInvoiceList);
+
+
+    List<Invoice> queryDate(String beginDate, String endDate,int page,int limit);
+}
+
+
